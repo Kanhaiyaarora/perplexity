76a5a8767998d0fa529054680533ac6359c992c7
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-03-18 14:43:01 +0530

    socket.io backend done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectToDb from "./src/config/database.js";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 connectToDb().catch((err) => {
   console.error("Failed to connect to MongoDB", err);
 });
 
-app.listen(3000, () => {
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
+httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
